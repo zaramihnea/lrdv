2f7d86af2020c5f19acdd54a78d630974dba9b5a
Zara Mihnea <zaramihnea@icloud.com>
2025-04-01 11:03:07 +0300

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -21,14 +21,17 @@ const HomePage: React.FC = () => {
 
   return (
     <>
-      <Helmet>
+      <Helmet htmlAttributes={{
+        lang: 'ro'
+      }}>
         <title>Literatura Română în Dimensiune Virtuală | Concurs Național Interdisciplinar</title>
         <meta name="description" content="Concurs național interdisciplinar organizat de Liceul Teoretic de Informatică Grigore Moisil Iași care îmbină literatura română cu tehnologia." />
+        <link rel="canonical" href="https://lrdv.netlify.app" />
         <meta property="og:title" content="Literatura Română în Dimensiune Virtuală" />
         <meta property="og:description" content="Concurs național interdisciplinar care valorifică performanțele elevilor de gimnaziu și liceu în utilizarea computerului și a potențialului creativ într-un demers personalizat de abordare a specificului cultural și literar național." />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://lrdv.netlify.app" />
-        <meta property="og:image" content="/images/banner_test.jpg" />
+        <meta property="og:image" content="https://lrdv.netlify.app/images/banner_test.jpg" />
       </Helmet>
       <div className="home-page">
         <Banner />
